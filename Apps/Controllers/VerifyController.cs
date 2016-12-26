efd1b340442b4f5ddebe75190a37a80972a23155
Johnny Yin <371711105@qq.com>
2016-12-26 21:43:13 +0800

    add verify code

---
+++ Apps/Controllers/VerifyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Apps.Core;
+
+namespace Apps.Controllers
+{
+    public class VerifyController : Controller
+    {
+        // GET: Verify
+        public ActionResult GetVerifyCode()
+        {
+            ValidateCode vCode = new ValidateCode();
+            string code = vCode.CreateValidateCode(5);
+            Session["ValidateCode"] = code;
+            byte[] bytes = vCode.CreateValidateGraphic(code);
+            return File(bytes, @"image/jpeg");
+        }
+    }
+}
