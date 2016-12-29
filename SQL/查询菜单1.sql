0d43b6a2fe77276fd68f521974f9ae7c96195e6e
Johnny Yin <371711105@qq.com>
2016-12-29 23:16:00 +0800

    add module

---
+++ SQL/查询菜单1.sql
@@ -0,0 +1,1 @@
+﻿select * from sysmodule where ParentId = '0'
--- SQL/添加权限.sql
+++ SQL/添加权限.sql
@@ -1,3 +1,16 @@
 ﻿use AppsDB
 
-insert into SysRight values ('administratorModuleSetting','ModuleSetting','administrator','True')
+--insert into SysRight values ('administratorSystemManage','SystemManage','administrator','True')
+--insert into SysRight values ('administratorModuleSetting','ModuleSetting','administrator','True')
+insert into SysRight values ('administratorUserManage','UserManage','administrator','True')
+insert into SysRight values ('administratorRoleManage','RoleManage','administrator','True')
+insert into SysRight values ('administratorRoleAuthorize','RoleAuthorize','administrator','True')
+insert into SysRight values ('administratorSystemConfig','SystemConfig','administrator','True')
+insert into SysRight values ('administratorSystemLog','SystemLog','administrator','True')
+insert into SysRight values ('administratorSystemExcepiton','SystemExcepiton','administrator','True')
+insert into SysRight values ('administratorDocument','Document','administrator','True')
+insert into SysRight values ('administratorInfo','Info','administrator','True')
+
+
+--insert into SysRight values ('administratorRightManage','RightManage','administrator','True')
+--insert into SysRight values ('administratorPersonDocument','PersonDocument','administrator','True')
